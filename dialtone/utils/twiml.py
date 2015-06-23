bfe4f6cef8ab3b83fd61590adc07396a59fb74f2
Michael Lavers <kolanos@gmail.com>
2015-06-22 20:25:37 -0700

    fix todict references

--- dialtone/utils/twiml.py
+++ dialtone/utils/twiml.py
@@ -5,16 +5,16 @@ def to_dict(obj, classkey=None):
     if isinstance(obj, dict):
         data = {}
         for (k, v) in obj.items():
-            data[k] = todict(v, classkey)
+            data[k] = to_dict(v, classkey)
         return data
     elif hasattr(obj, "_ast"):
-        return todict(obj._ast())
+        return to_dict(obj._ast())
     elif hasattr(obj, "__iter__"):
-        return [todict(v, classkey) for v in obj]
+        return [to_dict(v, classkey) for v in obj]
     elif hasattr(obj, "__dict__"):
-        data = dict([(key, todict(value, classkey))
-            for key, value in obj.__dict__.iteritems()
-            if not callable(value) and not key.startswith('_')])
+        data = dict([(key, to_dict(value, classkey))
+                    for key, value in obj.__dict__.iteritems()
+                    if not callable(value) and not key.startswith('_')])
         if classkey is not None and hasattr(obj, "__class__"):
             data[classkey] = obj.__class__.__name__
         return data
