af1d45cec82fe64a4141fe395592b96520249514
Michael Lavers <kolanos@gmail.com>
2015-07-05 09:40:45 -0700

    remove manage.py until i have something to manage.py

--- dialtone/config.py
+++ dialtone/config.py
@@ -6,8 +6,10 @@ from dialtone.utils import phone
 # Flask configuration
 #
 
-DEBUG = True
-SECRET_KEY = os.environ.get('SECREt_KEY', 'shhhh')
+DEBUG = True if os.environ.get('DEBUG') else False
+
+SECRET_KEY = os.environ.get('SECREt_KEY')
+assert SECRET_KEY, 'A SECRET_KEY environment variable is required.'
 
 #
 # Dialtone configuration
@@ -22,15 +24,23 @@ LANGUAGE = os.environ.get('LANGUAGE', 'en-US')
 
 # Number to use for outgoing phone calls
 OUTGOING_NUMBER = os.environ.get('OUTGOING_NUMBER')
+assert OUTGOING_NUMBER, 'An OUTGOING_NUMBER environment variable is required.'
 
 # List of phone numbers
 PHONES = map(phone.normalize, os.environ.get('PHONES', '').split(','))
-
-# Twilio account sid
-TWILIO_SID = os.environ.get('TWILIO_SID')
-
-# Twilio auth token
-TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+assert PHONES, 'A PHONES environment variable is required.'
 
 # Text-to-speech voice to use, default is recommended
 VOICE = os.environ.get('VOICE', 'alice')
+
+#
+# Twilio Configuration
+#
+
+# Twilio account sid
+TWILIO_SID = os.environ.get('TWILIO_SID')
+assert TWILIO_SID, 'A TWILIO_SID environment variable is rquired.'
+
+# Twilio auth token
+TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+assert TWILIO_TOKEN, 'A TWILIO_TOKEN environment variable is required.'
