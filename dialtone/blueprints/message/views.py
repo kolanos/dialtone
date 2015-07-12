10056d945537dabd2d85ec283c4853ff2c1035ee
Michael Lavers <kolanos@gmail.com>
2015-07-12 13:21:38 -0700

    use phonenumberslite instead of phonenumbers

--- dialtone/blueprints/message/views.py
+++ dialtone/blueprints/message/views.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
 
-bp = Blueprint('sms', __name__)
+bp = Blueprint('message', __name__)
+
+
+@bp.route('/twiml/message/', methods=['POST'])
+def message():
+    pass
 
 
 @bp.route('/twiml/message/fallback/', methods=['POST'])
@@ -8,6 +13,4 @@ def message_fallback():
     pass
 
 
-@bp.route('/twiml/message/', methods=['POST'])
-def message():
-    pass
+
